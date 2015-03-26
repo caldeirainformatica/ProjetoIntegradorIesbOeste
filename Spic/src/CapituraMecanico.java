86fdd79d84a6b031ad4160d6e7aed0b270867f19
RICARDO CALDEIRA LIMA <ricardocaldeiralima@gmail.com>
2015-03-26 19:01:39 -0300

    Update and rename Mecanico.java to CapituraMecanico.java

---
+++ Spic/src/CapituraMecanico.java
@@ -0,0 +1,9 @@
+
+public class Mecanico {
+
+  String nomeMecanico;
+  int idMecanico;
+  // tive que voltar ao trabalho, lembrar de incluir os demais campo e quando estiver no eclipse, criar os gets e sets.
+  
+  essa classe apenas vai pegar e guardar no array os dados de mecanicos, bem como tempo disponivel para o serviço
+}
