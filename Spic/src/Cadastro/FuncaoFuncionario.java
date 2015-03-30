ffe2df092edef43cbc4c25e3ec3fb36dc78d958a
RicardoNote <RicardoNote@RicardoNote-PC.iesb.br>
2015-03-30 20:55:26 -0300

    Criação da classe funçãofuncionario

---
+++ Spic/src/Cadastro/FuncaoFuncionario.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package Cadastro;
+
+/**
+ * @author RicardoNote
+ *
+ */
+public class FuncaoFuncionario {
+	private String descricaoFuncao;
+
+	/**
+	 * @param descricaoFuncao
+	 */
+	//Abaixo, metodo construtor da Classe
+	public FuncaoFuncionario(String descricaoFuncao) { 
+		super();
+		this.descricaoFuncao = descricaoFuncao;
+	}
+
+	/**
+	 * @return the descricaoFuncao
+	 */
+	public String getDescricaoFuncao() {
+		return descricaoFuncao;
+	}
+
+	/**
+	 * @param descricaoFuncao the descricaoFuncao to set
+	 */
+	public void setDescricaoFuncao(String descricaoFuncao) {
+		this.descricaoFuncao = descricaoFuncao;
+	}
+
+	
+	
+}
