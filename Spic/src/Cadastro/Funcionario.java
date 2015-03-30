164becae92c127fbaefeb2c425e4c877d1f085b5
Fiuza <Fiuza@Fiuza-PC.iesb.br>
2015-03-30 20:10:06 -0300

    atualização

--- Spic/src/Cadastro/Funcionario.java
+++ Spic/src/Cadastro/Funcionario.java
@@ -1,5 +1,9 @@
 package Cadastro;
 
 public class Funcionario {
+	
+	
+	
+	
 
 }
---
+++ Spic/src/Cadastro/Produto.java
@@ -0,0 +1,24 @@
+
+public class Produto {
+	
+	
+	private int codigoBarras;
+	private String descricao;
+	private String aplicacao;
+	private String medida;
+	private Boolean ativo;
+	private Date dataValidade;
+	private Date dataFabricacao;
+	private String lote;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
