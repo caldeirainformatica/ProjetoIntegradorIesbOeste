a6da6d5733df77dafd44dc1d7273bb2f37226ae3
RicardoNote <RicardoNote@RicardoNote-PC.iesb.br>
2015-03-30 20:00:33 -0300

    Atualização

---
+++ Spic/src/Cadastro/Servicos.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package Cadastro;
+
+/**
+ * @author Ricardo Caldeira
+ *
+ */
+public class Servicos {
+	
+	
+
+}
--- Spic/src/Cadastro/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author RicardoNote
- *
- */
-package Cadastro;
