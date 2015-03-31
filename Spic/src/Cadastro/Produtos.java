ad7c3298321ac37558323ea22171b4e98dd38d14
Fiuza <Fiuza@Fiuza-PC.iesb.br>
2015-03-30 22:11:25 -0300

    Criação da classe produtos,Criação da classe main SpicTeste

--- Spic/src/Cadastro/Produtos.java
+++ Spic/src/Cadastro/Produtos.java
@@ -11,6 +11,7 @@ import java.sql.Date;
  */
 public class Produtos {
 	
+	private int codigoBarras;
 	private String descricao;
 	private String aplicacao;
 	private String medida;
@@ -19,61 +20,9 @@ public class Produtos {
 	private Date dataFabricacao;
 	private String lote;
 	
-	//getters and setters
-	private int codigoBarras;
-	public int getCodigoBarras() {
-		return codigoBarras;
-	}
-	public void setCodigoBarras(int codigoBarras) {
-		this.codigoBarras = codigoBarras;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public String getAplicacao() {
-		return aplicacao;
-	}
-	public void setAplicacao(String aplicacao) {
-		this.aplicacao = aplicacao;
-	}
-	public String getMedida() {
-		return medida;
-	}
-	public void setMedida(String medida) {
-		this.medida = medida;
-	}
-	public Boolean getAtivo() {
-		return ativo;
-	}
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
-	}
-	public Date getDataValidade() {
-		return dataValidade;
-	}
-	public void setDataValidade(Date dataValidade) {
-		this.dataValidade = dataValidade;
-	}
-	public Date getDataFabricacao() {
-		return dataFabricacao;
-	}
-	public void setDataFabricacao(Date dataFabricacao) {
-		this.dataFabricacao = dataFabricacao;
-	}
-	public String getLote() {
-		return lote;
-	}
-	public void setLote(String lote) {
-		this.lote = lote;
-	}
-	
 	//método construtor
 	
 	public Produtos(int codigoBarras,String descricao,String aplicacao,String medida,Boolean ativo,Date dataValidade,Date dataFabricacao,String lote) {
-		
 		this.codigoBarras = codigoBarras;
 		this.descricao = descricao;
 		this.aplicacao = aplicacao;
@@ -82,6 +31,38 @@ public class Produtos {
 		this.dataValidade = dataValidade;
 		this.dataFabricacao = dataFabricacao;
 		this.lote = lote;
+	}
+
+	public int getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public String getAplicacao() {
+		return aplicacao;
+	}
+
+	public String getMedida() {
+		return medida;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public String getLote() {
+		return lote;
 	}
 	
 	
---
+++ Spic/src/Cadastro/SpicTeste.java
@@ -0,0 +1,9 @@
+package Cadastro;
+
+public class SpicTeste {
+	public static void main(String[] args) {
+		
+		Produtos mola = new Produtos(codigoBarras, descricao, aplicacao, medida, ativo, dataValidade, dataFabricacao, lote);
+	}
+
+}
