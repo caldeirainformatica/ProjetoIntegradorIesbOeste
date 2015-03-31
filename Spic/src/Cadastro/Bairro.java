d5d8d6b35c5b41139a2497a40334db789d1f595b
RicardoNote <RicardoNote@RicardoNote-PC.iesb.br>
2015-03-30 21:01:56 -0300

    Atualização das classes

---
+++ Spic/src/Cadastro/Bairro.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package Cadastro;
+
+/**
+ * @author Ricardo Caldeira
+ *
+ */
+public class Bairro {
+	private String descricaoBairro;
+
+	/**
+	 * @param descricaoBairro
+	 */
+	public Bairro(String descricaoBairro) {
+		super();
+		this.descricaoBairro = descricaoBairro;
+	}
+
+	/**
+	 * @return the descricaoBairro
+	 */
+	public String getDescricaoBairro() {
+		return descricaoBairro;
+	}
+
+	/**
+	 * @param descricaoBairro the descricaoBairro to set
+	 * Metodo construtor para a classe Bairro.
+	 */
+	public void setDescricaoBairro(String descricaoBairro) {
+		this.descricaoBairro = descricaoBairro;
+	}
+
+}
---
+++ Spic/src/Cadastro/Cidade.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package Cadastro;
+
+/**
+ * @author Ricardo Caldeira
+ *
+ */
+public class Cidade {
+	/**
+	 * @param cidade
+	 */
+	public Cidade(String cidade) {
+		super();
+		this.cidade = cidade;
+	}
+
+	private String cidade;
+
+	/**
+	 * @return the cidade
+	 */
+	public String getCidade() {
+		return cidade;
+	}
+
+	/**
+	 * @param cidade the cidade to set
+	 * Metodo construtor da classe Cidade
+	 */
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+
+}
