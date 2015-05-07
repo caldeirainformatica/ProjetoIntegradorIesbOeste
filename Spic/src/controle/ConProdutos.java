77b860cdc0bababf5e6370131b41fa812ad7db9d
Fiuza <Fiuza@Fiuza-PC>
2015-05-07 01:37:24 -0300

    Classes lote e ConLote estão prontas. Foram retirados os atributos dataValidade, dataFabricacao,ativo da classe Produtos.

--- Spic/src/controle/ConProdutos.java
+++ Spic/src/controle/ConProdutos.java
@@ -92,7 +92,7 @@ public class ConProdutos {
 
 	public boolean validarMedida(String medida){
 		
-		if(medida == ""||medida== null||medida.trim()==""){
+		if(medida == ""||medida== null||medida.trim()==""||medida.length()!=2){
 			
 			Exception campoVazioException = new Exception("Valor incorreto!");
 			return false;
@@ -106,112 +106,6 @@ public class ConProdutos {
 		}
 	}
 
-	public boolean validarAtivo(String ativo){
-		
-		if(ativo == ""||ativo== null||ativo.trim()==""){
-			
-			Exception campoVazioException = new Exception("Valor incorreto!");
-			return false;
-		
-		}else{
-			
-			
-			
-			return true;
-			
-		}
-		
-	}
-	
-	public boolean validarDataValidade(String dataValidade)throws java.text.ParseException{  
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");  
-        Calendar cal = new GregorianCalendar();  
-
-            // gerando o calendar  
-        cal.setTime(df.parse(dataValidade));  
-
-            // separando os dados da string para comparacao e validacao  
-        String[] data = dataValidade.split("/");  
-        String dia = data[0];  
-        String mes = data[1];  
-        String ano = data[2];  
-
-            // testando se hah discrepancia entre a data que foi  
-            // inserida no caledar e a data que foi passada como  
-            // string. se houver diferenca, a data passada era  
-            // invalida  
-        if ( (new Integer(dia)).intValue() != (new  
-        		Integer(cal.get(Calendar.DAY_OF_MONTH))).intValue() ) {  
-           // dia nao casou  
-            return(false);  
-        } else if ( (new Integer(mes)).intValue() != (new  
-        		Integer(cal.get(Calendar.MONTH)+1)).intValue() ) {  
-                // mes nao casou  
-            return(false);  
-        } else if ( (new Integer(ano)).intValue() != (new  
-        		Integer(cal.get(Calendar.YEAR))).intValue() ) {  
-                // ano nao casou  
-            return(false);  
-        }  
-
-        produto.setDataValidade(dataValidade);
-        return(true);                          
-    }  
-
-
-
-
-	public boolean validarDataFabricacao(String dataFabricacao)throws java.text.ParseException {  
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");  
-        Calendar cal = new GregorianCalendar();  
-
-            // gerando o calendar  
-        cal.setTime(df.parse(dataFabricacao));  
-
-            // separando os dados da string para comparacao e validacao  
-        String[] data = dataFabricacao.split("/");  
-        String dia = data[0];  
-        String mes = data[1];  
-        String ano = data[2];  
-
-            // testando se hah discrepancia entre a data que foi  
-            // inserida no caledar e a data que foi passada como  
-            // string. se houver diferenca, a data passada era  
-            // invalida  
-        if ( (new Integer(dia)).intValue() != (new  
-        		Integer(cal.get(Calendar.DAY_OF_MONTH))).intValue() ) {  
-           // dia nao casou  
-            return(false);  
-        } else if ( (new Integer(mes)).intValue() != (new  
-        		Integer(cal.get(Calendar.MONTH)+1)).intValue() ) {  
-                // mes nao casou  
-            return(false);  
-        } else if ( (new Integer(ano)).intValue() != (new  
-        		Integer(cal.get(Calendar.YEAR))).intValue() ) {  
-                // ano nao casou  
-            return(false);  
-        }  
-        produto.setDataFabricacao(dataFabricacao);
-        return(true);                          
-    }  
-
-
-
-	
-	public boolean validarLote(String lote){
-		if(lote == ""||lote== null||lote.trim()==""){
-			
-			
-			return false;
-		
-		}else{
-			
-			
-			produto.setLote(lote);
-			return true;
-			
-		}
-	}
 	
 	public boolean validarValorVenda(String valorVenda)throws Exception {
 		
