6e26dc259960a8a6e80be6f969c317f844949d44
Ezequiel_Henrique <ezequiel.henrrick31@gmail.com>
2015-05-13 01:37:32 -0300

    Fazendo para entender melhor a criação e implantação dos getters e setters

--- Spic/src/controle/Con_Lote_Ezeq.java
+++ Spic/src/controle/Con_Lote_Ezeq.java
@@ -1,58 +1,70 @@
-/**
- * 
- */
 package controle;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * @author Ricardo Caldeira
- * @author Gabriel Fernandes
- * @author Ezequiel Henrique
- *
- */
-public class ConLote {
+public class Con_Lote_Ezeq {
 
 	private int idLote;
 	private String descricao;
 	private Date dateValidade;
 	private Date dateFabricacao;
-	
-	
-	
+
 	public int getIdLote() {
 		return idLote;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public Date getDateValidade() {
 		return dateValidade;
 	}
+
 	public Date getDateFabricacao() {
 		return dateFabricacao;
 	}
+
 	public void setIdLote(int idLote) {
-		this.idLote = idLote;
+
+		try {
+			if (idLote > 0) {
+
+				this.idLote = idLote;
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
+
 	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+
+		try {
+			if (!descricao.equals("")) {
+
+				this.descricao = descricao;
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
+
 	public void setDateValidade(Date dateValidade) {
 		this.dateValidade = dateValidade;
-	}
+		
+		String x=request.setParameter("dataUsuario"); //pegando dados de um formulário WEB 
+		SimpleDateFormat sdf1= new SimpleDateFormat("dd/MM/yyyy"); 
+		Date dataUsuario=sdf1.parse(x);
+
+		}
+
 	public void setDateFabricacao(Date dateFabricacao) {
 		this.dateFabricacao = dateFabricacao;
 	}
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
