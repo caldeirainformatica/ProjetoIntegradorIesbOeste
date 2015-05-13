c2923146dc4bc22cbf4c4b63852d1d07af7d4be5
Ezequiel_Henrique <ezequiel.henrrick31@gmail.com>
2015-05-13 07:46:31 -0300

    Mundança no Teste_Con_Lote_Ezeq

--- Spic/src/testes/Teste_Con_Lote_Ezeq.java
+++ Spic/src/testes/Teste_Con_Lote_Ezeq.java
@@ -1,5 +1,7 @@
 package testes;
 
+import java.io.ObjectInputStream.GetField;
+
 import controle.Con_Lote_Ezeq;
 
 public class Teste_Con_Lote_Ezeq {
@@ -7,11 +9,11 @@ public class Teste_Con_Lote_Ezeq {
 	public static void main(String[] args) {
 		// Testando a classe Con_Lote_Ezeq
 		
-		Con_Lote_Ezeq dateValidade = new Con_Lote_Ezeq();
+		Con_Lote_Ezeq date = new Con_Lote_Ezeq();
 
-		dateValidade.setDateValidade("20/11/2015");
+		date.setDateValidade("20/11/2015");
 		
-		System.out.println(dateValidade);
+		System.out.println(date);
 	}
 
 }
