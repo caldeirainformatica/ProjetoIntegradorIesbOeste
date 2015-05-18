1eec489e87a42e9dd6e21f05cf0727e7b43c6485
Ricardo Caldeira <Ricardo Caldeira@Notebook>
2015-05-18 15:23:26 -0300

    Implantação do sistema de vendas no menu principal

--- Spic/src/view/FrmPrincipal.java
+++ Spic/src/view/FrmPrincipal.java
@@ -1,7 +1,8 @@
-
 package view;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -16,13 +17,23 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Dimension;
 
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.SystemColor;
+import java.awt.Component;
+
 /**
  *
  * @author Ricardo Caldeira
  */
 public class FrmPrincipal extends javax.swing.JFrame {
 
-	ConectaBanco conecta = new ConectaBanco(); // variavel global de conexão com o banco de dados
+	ConectaBanco conecta = new ConectaBanco(); // variavel global de conexão com
+												// o banco de dados
 	/**
 	 * 
 	 */
@@ -32,97 +43,101 @@ public class FrmPrincipal extends javax.swing.JFrame {
 	 * Creates new form FrmPrincipal
 	 */
 	public FrmPrincipal() {
-		setTitle("SPIC- Menu Principal");
-		setResizable(false);
-		setType(Type.UTILITY);
-		setBackground(Color.WHITE);
-		getContentPane().setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		getContentPane().setLayout(null);
+
+		JPanel pnlBotoes = new JPanel();
+		pnlBotoes.setBackground(SystemColor.inactiveCaption);
+		pnlBotoes.setBounds(0, 0, 1350, 75);
+		getContentPane().add(pnlBotoes);
+		pnlBotoes.setLayout(null);
+
+		JButton btnProdutos = new JButton("");
+		btnProdutos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FrmCadastroProdutos cadastroProdutos = new FrmCadastroProdutos();
+				cadastroProdutos.setResizable(false);
+				cadastroProdutos.setVisible(true);
+			}
+		});
+		btnProdutos.setIcon(new ImageIcon(FrmPrincipal.class
+				.getResource("/images/Windows_Tools_Icon_48.png")));
+		btnProdutos.setBounds(10, 11, 62, 53);
+		pnlBotoes.add(btnProdutos);
+
+		JButton btnVendas = new JButton("");
+		btnVendas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FrmVendas vendas = new FrmVendas();
+				vendas.setVisible(true);
+				vendas.setResizable(false);
+				vendas.setExtendedState(MAXIMIZED_BOTH);
+			}
+		});
+		btnVendas.setIcon(new ImageIcon(FrmPrincipal.class
+				.getResource("/images/Dollar.png")));
+		btnVendas.setBounds(85, 11, 62, 53);
+		pnlBotoes.add(btnVendas);
+
+		JButton btnSair = new JButton("");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				// Pergunta para o usuário se tem mesmo certeza se deseja sair
+				// ou nao
+				if (JOptionPane.showConfirmDialog(null,
+						"Tem certeza que deseja sair?", "Confirmar",
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+
+					conecta.desconecta();
+					System.exit(0);
+
+				} else {
+
+				}
+			}
+		});
+		btnSair.setIcon(new ImageIcon(FrmPrincipal.class
+				.getResource("/images/domesticos_2141_porta.gif")));
+		btnSair.setBounds(157, 11, 62, 53);
+		pnlBotoes.add(btnSair);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnCadastro = new JMenu("Cadastro");
+		menuBar.add(mnCadastro);
+
+		JMenuItem mntmProdutos = new JMenuItem("Produtos");
+		mnCadastro.add(mntmProdutos);
+
+		JMenu mnMovimentos = new JMenu("Movimentos");
+		menuBar.add(mnMovimentos);
+
+		JMenuItem mntmVendas = new JMenuItem("Vendas");
+		mnMovimentos.add(mntmVendas);
 		initComponents();
+
+		Container container = getContentPane();
+
+		JPanel panel = new JPanel();
+		panel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 73, 1350, 636);
+		getContentPane().add(panel);
 		
-		setLocation(500, 5);
-		setSize(200, 100);
-		setAlwaysOnTop(true);
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(FrmPrincipal.class.getResource("/images/Logo Spic.jpg")));
+		panel.add(label);
+		Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(tela.width, tela.height);  
+
 		conecta.conexao();
 
 	}
 
-	
 	private void initComponents() {
-
-		jButton1 = new javax.swing.JButton();
-
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-		jButton1.setIcon(new ImageIcon(FrmPrincipal.class.getResource("/images/Windows_Tools_Icon_48.png"))); // NOI18N
-		jButton1.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				jButton1ActionPerformed(evt);
-			}
-		});
-
-		JButton button = new JButton("");
-		button.addActionListener(new ActionListener() {
-			
-		
-			public void actionPerformed(ActionEvent arg0) {
-				//Pergunta para o usuário se tem mesmo certeza se deseja sair ou nao
-				if (JOptionPane.showConfirmDialog(null, "Tem certeza que deseja sair?", "Confirmar",
-						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-					
-					conecta.desconecta();
-					System.exit(0);
-					
-				} else {
-				    
-				}
-			
-			}
-		});
-		button.setIcon(new ImageIcon(FrmPrincipal.class
-				.getResource("/images/domesticos_2141_porta.gif")));
-
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-				getContentPane());
-		layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING)
-				.addGroup(
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(jButton1,
-										GroupLayout.PREFERRED_SIZE, 61,
-										GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addComponent(button,
-										GroupLayout.PREFERRED_SIZE, 69,
-										GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(608, Short.MAX_VALUE)));
-		layout.setVerticalGroup(layout
-				.createParallelGroup(Alignment.LEADING)
-				.addGroup(
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(
-										layout.createParallelGroup(
-												Alignment.LEADING)
-												.addComponent(
-														button,
-														Alignment.TRAILING,
-														GroupLayout.DEFAULT_SIZE,
-														GroupLayout.DEFAULT_SIZE,
-														Short.MAX_VALUE)
-												.addComponent(jButton1))
-								.addContainerGap(37, Short.MAX_VALUE)));
-		getContentPane().setLayout(layout);
-
-		pack();
 	}// </editor-fold>
-
-	private void jButton1ActionPerformed(ActionEvent evt) {
-		FrmCadastroProdutos cadastroProdutos = new FrmCadastroProdutos();
-		cadastroProdutos.setVisible(true);
-	
-		
-
-	}
 
 	/**
 	 * @param args
@@ -165,11 +180,10 @@ public class FrmPrincipal extends javax.swing.JFrame {
 		/* Create and display the form */
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				new FrmPrincipal().setVisible(true);
+				FrmPrincipal principal = new FrmPrincipal();
+
+				principal.setVisible(true);
 			}
 		});
 	}
-
-	// Variables declaration - do not modify
-	private javax.swing.JButton jButton1;
 }
